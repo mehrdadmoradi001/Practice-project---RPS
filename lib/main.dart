36de7df84eb69c532b777f2174f91a977017eff3
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-08-19 23:49:28 +0430

    1- Methode Random().nextInt 2- Solve the scrollbar challenge 3- StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Application());
+  runApp(GameApplication());
 }
 
-class Application extends StatelessWidget {
-  const Application({Key? key}) : super(key: key);
+class GameApplication extends StatefulWidget {
+  const GameApplication({Key? key}) : super(key: key);
+
+  @override
+  State<GameApplication> createState() => _GameApplicationState();
+}
+
+class _GameApplicationState extends State<GameApplication> {
+  int top = 1;
+  int button = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -19,25 +29,33 @@ class Application extends StatelessWidget {
           backgroundColor: Colors.deepOrange,
         ),
         backgroundColor: Colors.deepOrange[700],
-        body: SingleChildScrollView(
-          child: SafeArea(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Image(
-                  image: AssetImage('images/1.png'),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround, //-
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image(
+                image: AssetImage('images/$top.png'),
+                height: 100,
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    top = Random().nextInt(3) + 1; //0-2 -> 1-3
+                    button = Random().nextInt(3) + 1;
+                  });
+                },
+                child: Text(
+                  'شروع کن',
+                  style: TextStyle(fontSize: 20),
                 ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text('شروع کن',style: TextStyle(fontSize: 20),),
-                  style: TextButton.styleFrom(primary: Colors.white),
-                ),
-                Image(
-                  image: AssetImage('images/2.png'),
-                ),
-              ],
-            ),
+                style: TextButton.styleFrom(primary: Colors.white),
+              ),
+              Image(
+                image: AssetImage('images/$button.png'),
+                height: 100,
+              ),
+            ],
           ),
         ),
       ),
