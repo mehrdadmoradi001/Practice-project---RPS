5c5c1bd369bda515a544f1f42bcc9f1e268da524
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-10-18 21:13:34 +0330

    1- Add Scoring with Decoration.

---
+++ lib/constant.dart
@@ -0,0 +1,6 @@
+import 'package:hexcolor/hexcolor.dart';
+import 'package:flutter/material.dart';
+
+
+final Color topColor = HexColor("FFB200");
+final Color bottomColor = HexColor("FFF9CA");
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'dart:math';
+import 'package:flutter_rock_paper_scissor/constant.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 import 'package:flutter/material.dart';
 
@@ -16,6 +18,8 @@ class GameApplication extends StatefulWidget {
 class _GameApplicationState extends State<GameApplication> {
   int top = 1;
   int button = 1;
+  int numBottom = 0;
+  int numTop = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,16 +28,26 @@ class _GameApplicationState extends State<GameApplication> {
       theme: ThemeData(fontFamily: 'vazir'),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('سنگ   کاغذ   قیچی'),
+          title: Text(
+            'سنگ   کاغذ   قیچی',
+            style: TextStyle(color: Colors.blue[900]),
+          ),
           centerTitle: true,
-          backgroundColor: Colors.deepOrange,
+          backgroundColor: topColor,
         ),
-        backgroundColor: Colors.deepOrange[700],
+        backgroundColor: bottomColor,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround, //-
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              DecoratedBox(
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,color: Colors.orange[300]),
+                child: Text('$numTop',
+                    style: TextStyle(fontSize: 28),
+                    textAlign: TextAlign.center),
+              ),
               Image(
                 image: AssetImage('images/$top.png'),
                 height: 100,
@@ -43,18 +57,39 @@ class _GameApplicationState extends State<GameApplication> {
                   setState(() {
                     top = Random().nextInt(3) + 1; //0-2 -> 1-3
                     button = Random().nextInt(3) + 1;
+
+                    if (top == 1 && button == 3) {
+                      numTop = numTop + 1;
+                    } else if (top == 2 && button == 3) {
+                      numBottom = numBottom + 1;
+                    } else if (top == 3 && button == 1) {
+                      numBottom = numBottom + 1;
+                    } else if (top == 2 && button == 1) {
+                      numTop = numTop + 1;
+                    } else if (top == 3 && button == 2) {
+                      numTop = numTop + 1;
+                    } else if (top == 1 && button == 2) {
+                      numBottom = numBottom + 1;
+                    }
                   });
                 },
                 child: Text(
-                  'شروع کن',
+                  'بزن بریم',
                   style: TextStyle(fontSize: 20),
                 ),
-                style: TextButton.styleFrom(primary: Colors.white),
+                style: TextButton.styleFrom(primary: Colors.blue[900]),
               ),
               Image(
                 image: AssetImage('images/$button.png'),
                 height: 100,
               ),
+              DecoratedBox(
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,color: Colors.amber[100]),
+                child: Text('$numBottom',
+                    style: TextStyle(fontSize: 28),
+                    textAlign: TextAlign.center),
+              )
             ],
           ),
         ),
