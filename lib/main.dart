035213ba31cebd1b76b937c79593c2eb969072a5
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-10-20 12:55:46 +0330

    1- Add Image 2- Arrow along with increasing points

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,13 @@ class GameApplication extends StatefulWidget {
 
 class _GameApplicationState extends State<GameApplication> {
   int top = 1;
-  int button = 1;
+  int bottom = 1;
   int numBottom = 0;
   int numTop = 0;
-
+  var imgTop = 'arow';
+  var imgBottom = 'arow';
+  bool flagBottom = false;
+  bool flagTop = false;
 
   @override
   Widget build(BuildContext context) {
@@ -42,60 +45,91 @@ class _GameApplicationState extends State<GameApplication> {
             mainAxisAlignment: MainAxisAlignment.spaceAround, //-
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              DecoratedBox(
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,color: Colors.orange[300]),
-                child: Text('$numTop',
-                    style: TextStyle(fontSize: 28),
-                    textAlign: TextAlign.center),
-              ),
-              Image(
-                image: AssetImage('images/$top.png'),
-                height: 100,
-              ),
+              _Display(numTop, imgTop, flagTop),
+              _Image(top),
               TextButton(
                 onPressed: () {
                   setState(() {
                     top = Random().nextInt(3) + 1; //0-2 -> 1-3
-                    button = Random().nextInt(3) + 1;
+                    bottom = Random().nextInt(3) + 1;
 
-                    if (top == 1 && button == 3) {
+                    if (top == 1 && bottom == 3) {
                       numTop = numTop + 1;
-                    } else if (top == 2 && button == 3) {
+                      flagTop = true;
+                      flagBottom = false;
+                    } else if (top == 2 && bottom == 3) {
                       numBottom = numBottom + 1;
-                    } else if (top == 3 && button == 1) {
+                      flagBottom = true;
+                      flagTop = false;
+                    } else if (top == 3 && bottom == 1) {
                       numBottom = numBottom + 1;
-                    } else if (top == 2 && button == 1) {
+                      flagBottom = true;
+                      flagTop = false;
+                    } else if (top == 2 && bottom == 1) {
                       numTop = numTop + 1;
-                    } else if (top == 3 && button == 2) {
+                      flagTop = true;
+                      flagBottom = false;
+                    } else if (top == 3 && bottom == 2) {
                       numTop = numTop + 1;
-                    } else if (top == 1 && button == 2) {
+                      flagTop = true;
+                      flagBottom = false;
+                    } else if (top == 1 && bottom == 2) {
                       numBottom = numBottom + 1;
+                      flagBottom = true;
+                      flagTop = false;
                     }
                   });
                 },
                 child: Text(
                   'بزن بریم',
-                  style: TextStyle(fontSize: 20),
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w800),
                 ),
                 style: TextButton.styleFrom(primary: Colors.blue[900]),
               ),
-              Image(
-                image: AssetImage('images/$button.png'),
-                height: 100,
-              ),
-              DecoratedBox(
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,color: Colors.amber[100]),
-                child: Text('$numBottom',
-                    style: TextStyle(fontSize: 28),
-                    textAlign: TextAlign.center),
-              )
+              _Image(bottom),
+              _Display(numBottom, imgBottom, flagBottom),
             ],
           ),
         ),
       ),
     );
   }
-}
 
+  Widget _Image(int img) {
+    return Image(
+      image: AssetImage('images/$img.png'),
+      height: 100,
+    );
+  }
+
+  Widget _Display(int text, String image, bool flag) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 180),
+          child: Container(
+            width: 40,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.orange[300]),
+              child: Text('$text',
+                  style: TextStyle(fontSize: 28), textAlign: TextAlign.center),
+            ),
+          ),
+        ),
+        Visibility(
+          visible: flag,
+          child: Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: Image(
+                image: AssetImage('images/$image.png'),
+                width: 40,
+                height: 40,
+                alignment: Alignment.bottomRight),
+          ),
+        ),
+      ],
+    );
+  }
+}
